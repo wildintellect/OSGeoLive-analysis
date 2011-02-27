a2ec3e2bfe957969c00d312aa70176318a13135e
Alex Mandel <aimandel@ucdavis.edu>
2011-02-26 17:36:57 -0800

    Fix to r code, updates to proposal and IRB docs

--- AgGuide/surveys/survey1.r
+++ AgGuide/surveys/survey1.r
@@ -16,7 +16,7 @@ con <- dbConnect(m, dbname = "../PlacerAg.db",loadable.extensions = TRUE)
 plotranks <- function(){
 	#TODO add ColorBrewer
 	#d1 <- dbReadTable(con,"FeatureScore")
-	d1 <- dbGetQuery(con,'SELECT * FROM FeatureScore WHERE total > 0')
+	d1 <- dbGetQuery(con,'SELECT * FROM FeatureScore WHERE total > 0 ORDER BY Total')
 
 	png(file="FeatureScore.png", width=800, height=600, units="px")
 
@@ -27,14 +27,14 @@ plotranks <- function(){
 
 	png()
 	#d2 <- dbReadTable(con,"SearchScore")
-	d2 <- dbGetQuery(con,'SELECT * FROM SearchScore WHERE total > 0')
+	d2 <- dbGetQuery(con,'SELECT * FROM SearchScore WHERE total > 0 ORDER BY Total')
 
 	png(file="SearchScore.png", width=800, height=600, units="px")	
 
 	par(las=2) # make label text perpendicular to axis
 	par(mar=c(5,8,4,2)) # increase y-axis margin.
 	#barplot(d2$total, main="Search Rankings", horiz=TRUE, names.arg=d2$search)
-	barplot(d2$total, main="Search Rankings", horiz=TRUE, names.arg=c("Feature","Location","Seasonality","Product Group","Exact Product"), col=terrain.colors(5))
+	barplot(d2$total, main="Search Rankings", horiz=TRUE, names.arg=c("Features","Location","Seasonality","Product Group","Exact Product"), col=terrain.colors(5))
 	dev.off()
 }
 
